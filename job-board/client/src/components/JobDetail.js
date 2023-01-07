73161b2ad677291fc8dcb26784419655a499206c
123-code <naranjojose256@gmail.com>
2023-01-07 16:38:35 -0500

    company GraphQL type

--- job-board/client/src/components/JobDetail.js
+++ job-board/client/src/components/JobDetail.js
@@ -1,9 +1,27 @@
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
-import { jobs } from '../fake-data';
+import { useState,useEffect } from 'react';
+import {getJob} from '../graphql/queries';
+
+
 
 function JobDetail() {
+  const[jobs,setjobs] = useState(null)
   const { jobId } = useParams();
+
+  useEffect(()=>{
+    getJob(jobId).then((data)=>{
+setjobs(data)
+    })
+  },[jobId])
+
+  if(!jobs){
+    return(
+      <>
+      <p> loading...</p>
+      </>
+    )
+  }
 
   const job = jobs.find((job) => job.id === jobId);
   return (
--- job-board/client/src/graphql/queries.js
+++ job-board/client/src/graphql/queries.js
@@ -1,6 +1,27 @@
 //yarn add graphql-request
 import {request,gql } from 'graphql-request';
 const GRAPHQL_SERVER_URL = 'http://localhost:9000/graphql'
+
+export async function getJob(id){
+  const query = gql`
+  query JobQuery($id:ID!){
+   job(id:$id) {
+     id
+     title
+     company{
+      id
+       name
+     }
+     description
+   }
+  }
+  `;
+  const variables = { id };
+  const {job} = await request(GRAPHQL_SERVER_URL,query,variables);
+  return job;
+  
+  }
+
 export async function getJobs(){
 const query = gql`
 query{
